638d2ffa7ef1322e464d27b211e304774bb9e026
LeungAllan <42834824+LeungAllan@users.noreply.github.com>
2018-09-06 18:34:07 +0800

    Add files via upload

--- Coursetro.sol
+++ Coursetro.sol
@@ -4,10 +4,26 @@ contract Coursetro {
     
    string fName;
    uint age;
+   address owner; 
    
-   function setInstructor(string _fName, uint _age) public {
+   constructor() public {     // Add this constructor
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    event Instructor(
+       string name,
+       uint age
+    );
+    
+   function setInstructor(string _fName, uint _age) onlyOwner public {
        fName = _fName;
        age = _age;
+       emit Instructor(_fName, _age); 
    }
    
    function getInstructor() public constant returns (string, uint) {
